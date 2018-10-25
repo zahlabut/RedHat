75b8d0eea29fc7c84920fb9d40afffe68ac7bc2c
Arkady Shtempler <ashtempl@redhat.com>
2018-10-25 15:29:41 +0300

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -7,8 +7,10 @@ controllers = exec_command_line_command('openstack server list --name controller
 controller_ips = [item['networks'].split('=')[-1] for item in controllers]
 
 for ip in controller_ips:
+    print ip
     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
     ssh_object.ssh_connect_key()
     ironics=ssh_object.ssh_command('docker ps | grep -i ironic')
+    for i in ironics:
+        print i
     ssh_object.ssh_close()
-    spec_print([ip,str(ironics)])
