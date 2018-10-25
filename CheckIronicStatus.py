b6f6f880d2b67f060f7e3b68c20e9ca2175c182b
Arkady Shtempler <ashtempl@redhat.com>
2018-10-25 15:26:04 +0300

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -6,19 +6,8 @@ print exec_command_line_command('source /home/stack/overcloudrc')
 controllers = exec_command_line_command('openstack server list --name controller -f json')['JsonOutput']
 controller_ips = [item['networks'].split('=')[-1] for item in controllers]
 
-
-### Check The Ironic Status ###
-# for ip in controller_ips:
-#     print 'ssh heat-admin@'+ip+'; docker ps | grep -i ironic; exit'
-#     print exec_command_line_command('ssh heat-admin@'+ip+'; docker ps | grep -i ironic; exit')['CommandOutput']
-
-
 for ip in controller_ips:
-    print ip
     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
     ssh_object.ssh_connect_key()
-    print ssh_object.ssh_command('date')
+    spec_print([ip,ssh_object.ssh_command('docker ps | grep -i ironic')])
     ssh_object.ssh_close()
-
-
- #ssh heat-admin@192.168.24.9 -i /home/stack/.ssh/id_rsa
