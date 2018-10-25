52afd64f9c785ca3f7df7a7a1f4725c4bc7a80e2
Arkady Shtempler <ashtempl@redhat.com>
2018-10-25 14:49:42 +0300

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -8,9 +8,14 @@ controller_ips = [item['networks'].split('=')[-1] for item in controllers]
 
 
 ### Check The Ironic Status ###
-for ip in controller_ips:
-    print 'ssh heat-admin@'+ip+'; docker ps | grep -i ironic; exit'
-    print exec_command_line_command('ssh heat-admin@'+ip+'; docker ps | grep -i ironic; exit')['CommandOutput']
+# for ip in controller_ips:
+#     print 'ssh heat-admin@'+ip+'; docker ps | grep -i ironic; exit'
+#     print exec_command_line_command('ssh heat-admin@'+ip+'; docker ps | grep -i ironic; exit')['CommandOutput']
 
+
+for ip in controller_ips:
+    ssh_object = SSH(host=ip,user='heat-admin',password='')
+    ssh_object.ssh_command('date')
+    SSH.ssh_close()
 
 
