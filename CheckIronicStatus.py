d128f9c9ba12c33b61801339f04f1dfeea014f70
Arkady Shtempler <ashtempl@redhat.com>
2018-10-25 13:57:00 +0300

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -1,5 +1,7 @@
 from Common import *
 
+print "Make sure you are sourced to Overcloud"
+print "Run source /home/stack/overcloudrc"
 exec_command_line_command('source /home/stack/overcloudrc')
 exec_command_line_command('openstack server list')
 
