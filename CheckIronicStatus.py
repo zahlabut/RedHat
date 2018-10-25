9a099ea512c192688e8d21487c37190279f3f59f
Arkady Shtempler <ashtempl@redhat.com>
2018-10-25 14:06:14 +0300

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -3,6 +3,9 @@ from Common import *
 print "Make sure you are sourced to Overcloud"
 print "Run source /home/stack/overcloudrc"
 print exec_command_line_command('source /home/stack/overcloudrc')
-print exec_command_line_command('openstack server list -f json')['JsonOutput']
+controllers = exec_command_line_command('openstack server list -f json --name controller')['JsonOutput']
+
+controller_ips = [ip for ip in controllers['JsonOutput']['Networks']]
+print controller_ips
 
 
