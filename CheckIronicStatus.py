58783b8b017650723a5062feecb5e49356761552
Arkady Shtempler <ashtempl@redhat.com>
2018-10-28 11:37:03 +0200

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -6,18 +6,25 @@ print exec_command_line_command('source /home/stack/overcloudrc')
 controllers = exec_command_line_command('openstack server list --name controller -f json')['JsonOutput']
 controller_ips = [item['networks'].split('=')[-1] for item in controllers]
 
+# # Check Ironoic on Undercloud#
+# for ip in controller_ips:
+#     print ip
+#     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
+#     ssh_object.ssh_connect_key()
+#     ironics=ssh_object.ssh_command('sudo docker ps | grep -i ironic')
+#     for k in ironics.keys():
+#         print k, '-->', ironics[k]
+#     ssh_object.ssh_close()
+
+
+# Check Ironic on Overcloud #
+print exec_command_line_command('openstack catalog show ironic')
+controller_command="for i in ironic_pxe_http ironic_pxe_tftp ironic_neutron_agent ironic_conductor ironic_api; do sudo docker ps|grep $i; done"
 for ip in controller_ips:
     print ip
     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
     ssh_object.ssh_connect_key()
-    ironics=ssh_object.ssh_command('sudo docker ps | grep -i ironic')
-    #print ironics
-    #print type(ironics)
-    #print ironics.keys()
+    ironics=ssh_object.ssh_command(controller_command)
     for k in ironics.keys():
         print k, '-->', ironics[k]
-    # #for k in ironics.iteritems():
-    # #    print k,'-->',ironics[k]
-    # for i in str(ironics).split('\r'):
-    #     print i
     ssh_object.ssh_close()
