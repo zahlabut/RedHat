bcccc9c5635a301c18c1dfbee889868d06f1f98c
Arkady Shtempler <ashtempl@redhat.com>
2018-10-28 17:48:51 +0200

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -1,20 +1,8 @@
 from Common import *
 
 ### Get controllers IPs ###
-spec_print(["Make sure you are sourced to Undercloud","Run source /home/stack/stackrc"])
 controllers = exec_command_line_command('source /home/stack/stackrc;openstack server list --name controller -f json')['JsonOutput']
 controller_ips = [item['networks'].split('=')[-1] for item in controllers]
-
-# # Check Ironoic on Undercloud#
-# for ip in controller_ips:
-#     print ip
-#     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
-#     ssh_object.ssh_connect_key()
-#     ironics=ssh_object.ssh_command('sudo docker ps | grep -i ironic')
-#     for k in ironics.keys():
-#         print k, '-->', ironics[k]
-#     ssh_object.ssh_close()
-
 
 # Check Ironic on Overcloud + ERRORs in logs #
 spec_print(['Check Ironic on Overcloud + ERRORs in logs'])
@@ -25,7 +13,7 @@ ironic_status= "for i in ironic_pxe_http ironic_pxe_tftp ironic_neutron_agent ir
 ironic_errors='grep -i error /var/log/containers/ironic/*'
 commands_to_execute=[ironic_status,ironic_errors]
 for ip in controller_ips:
-    print '---',ip,'---'
+    spec_print([ip])
     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
     ssh_object.ssh_connect_key()
     for com in commands_to_execute:
@@ -35,7 +23,6 @@ for ip in controller_ips:
             print k, '-->', com_output[k]
     ssh_object.ssh_close()
 
-
 # Check Network Ansible (neutron_api) + ERRORs in logs
 spec_print(['Check Network Ansible (neutron_api) + ERRORs in logs'])
 net_ans_status= "sudo docker ps | grep -i neutron_api"
@@ -44,7 +31,7 @@ expected_message='cat /var/log/containers/neutron/server.log* | grep -i networki
                  'zcat /var/log/containers/neutron/server.log* | grep -i networking_ansible.config'
 commands_to_execute=[net_ans_status,net_ans_errors,expected_message]
 for ip in controller_ips:
-    print '---',ip,'---'
+    spec_print([ip])
     ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
     ssh_object.ssh_connect_key()
     for com in commands_to_execute:
