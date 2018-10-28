72ca91671c375b553df2f5624b00fe9db986bf76
Arkady Shtempler <ashtempl@redhat.com>
2018-10-28 11:44:10 +0200

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -2,7 +2,7 @@ from Common import *
 
 ### Get controllers IPs ###
 spec_print(["Make sure you are sourced to Undercloud","Run source /home/stack/stackrc"])
-print exec_command_line_command('source /home/stack/overcloudrc')
+#print exec_command_line_command('source /home/stack/overcloudrc')
 controllers = exec_command_line_command('openstack server list --name controller -f json')['JsonOutput']
 controller_ips = [item['networks'].split('=')[-1] for item in controllers]
 
