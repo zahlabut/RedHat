efee58dc8dcd61cfab0ac46892086602b7d0f16f
Arkady Shtempler <ashtempl@redhat.com>
2018-10-28 18:25:50 +0200

    First push

--- CheckIronicStatus.py
+++ CheckIronicStatus.py
@@ -4,6 +4,11 @@ from Common import *
 controllers = exec_command_line_command('source /home/stack/stackrc;openstack server list --name controller -f json')['JsonOutput']
 controller_ips = [item['networks'].split('=')[-1] for item in controllers]
 
+### Get Ceph IPs ###
+cephs = exec_command_line_command('source /home/stack/stackrc;openstack server list --name cephstorage -f json')['JsonOutput']
+cephs_ips = [item['networks'].split('=')[-1] for item in cephs]
+
+
 # Check Ironic on Overcloud + ERRORs in logs #
 spec_print(['Check Ironic on Overcloud + ERRORs in logs'])
 catalog_output=exec_command_line_command('source /home/stack/overcloudrc;openstack catalog show ironic -f json')
@@ -41,5 +46,23 @@ for ip in controller_ips:
             print k, '-->', com_output[k]
     ssh_object.ssh_close()
 
+
+# Check Ceph Status + ERRORs in logs #
+spec_print(['Check Ceph Status + ERRORs in logs'])
+ceph_status= "cinder server-list | grep ceph"
+net_ans_errors='grep -i error /var/log/containers/neutron/server.log*'
+expected_message='cat /var/log/containers/neutron/server.log* | grep -i networking_ansible.config; ' \
+                 'zcat /var/log/containers/neutron/server.log* | grep -i networking_ansible.config'
+commands_to_execute=[net_ans_status,net_ans_errors,expected_message]
+for ip in controller_ips:
+    spec_print([ip])
+    ssh_object = SSH(ip,user='heat-admin',key_path='/home/stack/.ssh/id_rsa')
+    ssh_object.ssh_connect_key()
+    for com in commands_to_execute:
+        print '-->',com
+        com_output=ssh_object.ssh_command(com)
+        for k in com_output.keys():
+            print k, '-->', com_output[k]
+    ssh_object.ssh_close()
 
 
