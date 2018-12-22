84c84445845e3b285715720d242d875bb1649f7e
Arkady Shtempler <ashtempl@redhat.com>
2018-12-22 14:37:00 +0200

    First push

--- CheckSetup.py
+++ CheckSetup.py
@@ -5,7 +5,7 @@ import unittest
 overclud_user='heat-admin'
 overcloud_ssh_key='/home/stack/.ssh/id_rsa'
 bare_metal_guest_ports=['xe-0/0/6','xe-0/0/7']
-
+conf_switch_file = 'sw_conf.json'
 
 
 ### Get controllers IPs ###
@@ -18,16 +18,14 @@ cephs = exec_command_line_command('source /home/stack/stackrc;openstack server l
     'JsonOutput']
 cephs_ips = [item['networks'].split('=')[-1] for item in cephs]
 
-class Monolithic(unittest.TestCase):
+class AnsibleNetworkingFunctionalityTests(unittest.TestCase):
     # Check Ironic on Overcloud + ERRORs in logs #
     def test_ironic_in_catalog(self):
-        print 1
         #spec_print(['Check Ironic on Overcloud + ERRORs in logs'])
         catalog_output=exec_command_line_command('source /home/stack/overcloudrc;openstack catalog show ironic -f json')
         self.assertEqual(catalog_output['JsonOutput']['name'], 'ironic','Failed: ironic was not found in catalog output')
 
     def test_ironic_dockers_status(self):
-        print 2
         ironic_dockers=['ironic_pxe_http','ironic_pxe_tftp','ironic_neutron_agent','ironic_conductor','ironic_api']
         for ip in controller_ips:
             ssh_object = SSH(ip,user=overclud_user,key_path=overcloud_ssh_key)
@@ -40,7 +38,6 @@ class Monolithic(unittest.TestCase):
             ssh_object.ssh_close()
 
     def test_errors_in_ironic_logs(self):
-        print 3
         command='sudo grep -R ERROR /var/log/containers/ironic/*'
         for ip in controller_ips:
             ssh_object = SSH(ip, user=overclud_user, key_path=overcloud_ssh_key)
@@ -50,7 +47,6 @@ class Monolithic(unittest.TestCase):
             self.assertNotIn('ERROR', output, 'Failed: ' + ip + ' ERROR detected in log\n'+output)
 
     def test_dockers_neutron_api_status(self):
-        print 4
         for ip in controller_ips:
             ssh_object = SSH(ip,user=overclud_user,key_path=overcloud_ssh_key)
             ssh_object.ssh_connect_key()
@@ -61,7 +57,6 @@ class Monolithic(unittest.TestCase):
             self.assertIn('neutron_api', output, 'Failed: neutron_api is not running')
 
     def test_errors_in_neutron_api(self):
-        print 5
         command='grep -i error /var/log/containers/neutron/server.log*'
         for ip in controller_ips:
             ssh_object = SSH(ip, user=overclud_user, key_path=overcloud_ssh_key)
@@ -71,7 +66,6 @@ class Monolithic(unittest.TestCase):
             self.assertNotIn('ERROR', output, 'Failed: ' + ip + ' ERROR detected in log\n'+output)
 
     def test_net_ansible_indication_msg_in_log(self):
-        print 6
         commands=["grep 'networking_ansible.config' /var/log/containers/neutron/server.log* | grep 'Ansible Host'",
                   "zgrep 'networking_ansible.config' /var/log/containers/neutron/server.log* | grep 'Ansible Host'"]
         output = []
@@ -88,7 +82,6 @@ class Monolithic(unittest.TestCase):
                       ' no indication for Ansible Networking configuration in log'+'\n'+str(output)+'\n'+str(stderr))
 
     def test_check_ceph_status(self):
-        print 7
         ceph_status= "source /home/stack/overcloudrc; cinder service-list | grep ceph"
         out = exec_command_line_command(ceph_status)['CommandOutput']
         self.assertIn('ceph',out,'Failed: ceph is not running')
@@ -99,16 +92,12 @@ class Monolithic(unittest.TestCase):
         ssh_object.ssh_close()
         self.assertIn('HEALTH_OK',com_output,'Failed: "HEALTH_OK" not found in output of \n'+ceph_status+' command')
 
-    #
-    #
-    #
-    # ### Check Switch Vlans ###
-    # conf_data_file='sw_conf.json'
-    # exec_command_line_command("sshpass -p N3tAutomation! ssh ansible@10.9.95.25 'show configuration | display json' > "+conf_data_file)
-    # interface_vlan=juniper_config_parser(conf_data_file)['InterfaceVlan']
-    # for k in interface_vlan.keys():
-    #     print k,'-->',interface_vlan[k]
-
+    def test_switch_no_vlans_for_bm_ports(self):
+        exec_command_line_command("sshpass -p N3tAutomation! ssh ansible@10.9.95.25 'show configuration | display json' > "+conf_data_file)
+        interface_vlan=juniper_config_parser(conf_data_file)['InterfaceVlan']
+        for k in interface_vlan.keys():
+            for port in bare_metal_guest_ports:
+                self.assertNotIn(interface_vlan[k],port,'Failed: '+port+' was found as configurd\n'+interface_vlan[k])
 
 if __name__ == '__main__':
     unittest.main()
