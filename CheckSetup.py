5f80dc0317d8edda2ada4536a02565d3f48857b7
Arkady Shtempler <ashtempl@redhat.com>
2018-12-22 15:04:13 +0200

    First push

--- CheckSetup.py
+++ CheckSetup.py
@@ -4,7 +4,7 @@ import unittest
 ### Parameters ###
 overclud_user='heat-admin'
 overcloud_ssh_key='/home/stack/.ssh/id_rsa'
-bare_metal_guest_ports=['xe-0/0/6','xe-0/0/7']
+bare_metal_guest_ports=['xe-0/0/11']
 conf_switch_file = 'sw_conf.json'
 
 
