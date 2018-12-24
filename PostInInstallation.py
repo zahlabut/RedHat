3c2711417090bd02f3ad860f949e357d343fe1c7
Arkady Shtempler <ashtempl@redhat.com>
2018-12-24 13:26:54 +0200

    First push

--- PostInInstallation.py
+++ PostInInstallation.py
@@ -90,26 +90,16 @@ if 'small' not in existing_flavors:
 
 # Create bm-deploy-kernel image #
 if 'bm-deploy-kernel' not in existing_images:
-    command=source_command+'openstack image create --container-format aki  --disk-format aki --public --file /home/stack/ironic-python-agent.kernel bm-deploy-kernel -f json'
-    print command
-    kernel_id=exec_command_line_command(command)
-    print kernel_id
-    print ram_id.keys()
+    kernel_id=exec_command_line_command(source_command+'openstack image create --container-format aki  --disk-format aki --public --file /home/stack/ironic-python-agent.kernel bm-deploy-kernel -f json')['JsonOutput']['id']
 
-# # Create bm-deploy-ramdisk image #
-# if 'bm-deploy-ramdisk' not in existing_images:
-#     ram_id=exec_command_line_command(source_command+'openstack image create --container-format ari  --disk-format ari --public  --file /home/stack/ironic-python-agent.initramfs bm-deploy-ramdisk -f json')['JsonOutput']#['id']
-#
-#
-# print ram_id
-#
-# print kernel_id.keys()
+# Create bm-deploy-ramdisk image #
+if 'bm-deploy-ramdisk' not in existing_images:
+    ram_id=exec_command_line_command(source_command+'openstack image create --container-format ari  --disk-format ari --public  --file /home/stack/ironic-python-agent.initramfs bm-deploy-ramdisk -f json')['JsonOutput']['id']
+    # Associate image per BM Guest #
+    exec_command_line_command(source_command+'openstack baremetal node set ironic-0 --driver-info deploy_kernel='+kernel_id+' --driver-info deploy_ramdisk='+ram_id)
+    exec_command_line_command(source_command+'openstack baremetal node set ironic-1 --driver-info deploy_kernel='+kernel_id+' --driver-info deploy_ramdisk='+ram_id)
 
 
-
-# Associate image per BM Guest #
-#exec_command_line_command(source_command+'openstack baremetal node set ironic-0 --driver-info deploy_kernel='+kernel_id+' --driver-info deploy_ramdisk='+ram_id)
-#exec_command_line_command(source_command+'openstack baremetal node set ironic-1 --driver-info deploy_kernel='+kernel_id+' --driver-info deploy_ramdisk='+ram_id)
 
 
 
