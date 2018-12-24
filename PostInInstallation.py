146cb62b8b8a66094ff9d4d5399d78e4b46d9f28
Arkady Shtempler <ashtempl@redhat.com>
2018-12-24 12:26:05 +0200

    First push

--- PostInInstallation.py
+++ PostInInstallation.py
@@ -2,17 +2,17 @@ from Common import *
 source_command='source /home/stack/overcloudrc;'
 
 existing_baremetal_nodes=[item['name'] for item in exec_command_line_command(source_command+'openstack baremetal node list -f json')['JsonOutput']]
-print existing_baremetal_nodes
+print 'BareMetals --> ',existing_baremetal_nodes
 existing_networks=[item['name'] for item in exec_command_line_command(source_command+'openstack network list -f json')['JsonOutput']]
-print existing_networks
+print 'Networks --> ',existing_networks
 existing_subnets=[item['name'] for item in exec_command_line_command(source_command+'openstack subnet list -f json')['JsonOutput']]
-print existing_subnets
+print 'Subnets --> ',existing_subnets
 existing_routers=[item['name'] for item in exec_command_line_command(source_command+'openstack router list -f json')['JsonOutput']]
-print existing_routers
+print 'Routers --> ',existing_routers
 existing_images=[item['name'] for item in exec_command_line_command(source_command+'openstack image list -f json')['JsonOutput']]
-print existing_images
+print 'images --> ',existing_images
 existing_flavors=[item['name'] for item in exec_command_line_command(source_command+'openstack flavor list -f json')['JsonOutput']]
-print existing_flavors
+print 'Flavors --> ',existing_flavors
 
 
 # Import BM nodes #
@@ -37,7 +37,7 @@ if 'dummy-subnet' not in existing_subnets:
     exec_command_line_command(source_command+'openstack subnet create --network dummy --subnet-range 192.168.24.0/24 --ip-version 4 --gateway 192.168.24.111 --allocation-pool start=192.168.24.30,end=192.168.24.50 --no-dhcp dummy-subnet')
 
 # Create ProvisionRouter router #
-if 'ProvisionRouter' not in existing_routers:
+if 'ProvisionRouter'.lower() not in existing_routers:
     commands=['openstack router create ProvisionRouter',
               'openstack router add subnet ProvisionRouter baremetal-subnet',
               'openstack router add subnet ProvisionRouter dummy-subnet']
