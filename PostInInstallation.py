d79549ae3d0c46f25266071a6a4b45c7971780b4
Arkady Shtempler <ashtempl@redhat.com>
2018-12-24 11:12:29 +0200

    First push

--- PostInInstallation.py
+++ PostInInstallation.py
@@ -1,5 +1,9 @@
 from Common import *
 
 # Import BM nodes #
-import_bm_nodes_command='source /home/stack/overcloudrc; openstack baremetal create bm_guests_env.yaml'
-print exec_command_line_command(import_bm_nodes_command)
+check_if_bm_exists='source /home/stack/overcloudrc; openstack baremetal node list -f json'
+result=exec_command_line_command(check_if_bm_exists)
+print result
+
+#import_bm_nodes_command='source /home/stack/overcloudrc; openstack baremetal create bm_guests_env.yaml'
+#print exec_command_line_command(import_bm_nodes_command)
