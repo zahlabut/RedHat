9d01149c01dc927d49750f4d2c6157c9178c15ac
Arkady Shtempler <ashtempl@redhat.com>
2018-12-24 11:40:06 +0200

    First push

--- PostInInstallation.py
+++ PostInInstallation.py
@@ -1,18 +1,9 @@
 from Common import *
 
-ironic_names=['ironic-0','ironic-1'] #As hardcoded in bm_guests_env.yaml file
-
 # Import BM nodes #
-to_import=[]
 check_if_bm_exists='source /home/stack/overcloudrc; openstack baremetal node list -f json'
 result=exec_command_line_command(check_if_bm_exists)
 if result['JsonOutput']!=None:
-    names=[]
-    for item in result['JsonOutput']:
-        print item['name']
-        if item['name'] not in ironic_names:
-            to_import.append(True)
-print to_import
-# if True in to_import or len(to_import) in [0,1]:
-#     import_bm_nodes_command='source /home/stack/overcloudrc; openstack baremetal create bm_guests_env.yaml'
-#     print exec_command_line_command(import_bm_nodes_command)
+    if len(result['JsonOutput'])!=2:
+        import_bm_nodes_command='source /home/stack/overcloudrc; openstack baremetal create bm_guests_env.yaml'
+        print exec_command_line_command(import_bm_nodes_command)
