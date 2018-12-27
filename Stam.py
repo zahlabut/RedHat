683ce4709a89c6b82f2e0d18de471c251b650ace
Arkady Shtempler <ashtempl@redhat.com>
2018-12-27 19:11:47 +0200

    First push

--- Stam.py
+++ Stam.py
@@ -5,7 +5,7 @@ switch_password='N3tAutomation!'
 
 ssh_object=SSH(switch_ip,switch_user,switch_password)
 ssh_object.ssh_connect_password()
-out= ssh_object.ssh_command('show configuration | display json')
+out= str(ssh_object.ssh_command('show configuration | display json'))
 print out
 f=open('stam','w')
 f.write(out)
