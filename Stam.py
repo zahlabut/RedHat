259e00ac8b02135f249d7cebffb6c90d2d21c6a1
Arkady Shtempler <ashtempl@redhat.com>
2018-12-27 18:53:57 +0200

    First push

--- Stam.py
+++ Stam.py
@@ -3,7 +3,7 @@ switch_ip='10.9.95.25'
 switch_user='ansible'
 switch_password='N3tAutomation!'
 
-ssh_object=SSH()
-ssh_object.ssh_connect_password(switch_ip,switch_user,switch_password)
+ssh_object=SSH(switch_ip,switch_user,switch_password)
+ssh_object.ssh_connect_password()
 print ssh_object.ssh_command('show configuration | display json')
 ssh_object.ssh_close()
