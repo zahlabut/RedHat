56e71a54e569614b8a7cda4cd8f4fae1adb291c8
Arkady Shtempler <ashtempl@redhat.com>
2018-12-27 19:40:51 +0200

    First push

--- CheckSetup.py
+++ CheckSetup.py
@@ -135,7 +135,7 @@ class AnsibleNetworkingFunctionalityTests(unittest.TestCase):
             if len(actual_vlans)==2:
                 to_stop=True
             time.sleep(3)
-        self.assertEqual(actual_vlans,[[baremetal_vlan_id],[baremetal_vlan_id]], 'Failed: baremetal ports are set to incorrect vlans:\n'+str(actual_vlans))
+        self.assertEqual(str(actual_vlans).count(baremetal_vlan_id),2, 'Failed: baremetal ports are set to incorrect vlans:\n'+str(actual_vlans))
 
 
     # def create_and_delete_bm_guest(self):
