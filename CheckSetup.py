006caced7d3c02cb148953710ec89e5e4c2dbe29
Arkady Shtempler <ashtempl@redhat.com>
2018-12-31 21:45:57 +0200

    First push

--- CheckSetup.py
+++ CheckSetup.py
@@ -28,7 +28,7 @@ virt_setup_parameters={
     'baremetal_guest_ports':['xe-0/0/7', 'xe-0/0/8'],
     'switch_type':'juniper_emulator_sw',
     'switch_ip':'172.16.0.92',
-    'switch_user':'root',
+    'switch_user':'ansible',
     'switch_password':'Juniper',
     'tenant_nets':['tempest-shared'],
     'setup':'Virtual_Setup'
