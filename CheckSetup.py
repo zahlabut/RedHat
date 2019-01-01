c1abd9eec62303e38e8b9fda6e44cf20b8ac5bfb
Arkady Shtempler <ashtempl@redhat.com>
2019-01-01 18:32:31 +0200

    First push

--- CheckSetup.py
+++ CheckSetup.py
@@ -154,7 +154,8 @@ class AnsibleNetworkingFunctionalityTests(unittest.TestCase):
             exec_command_line_command(source_overcloud+'openstack baremetal node provide '+id)
         start_time=time.time()
         to_stop=False
-        while to_stop==False or (time.time()>(start_time+manageable_timeout)):
+        while to_stop or (time.time()>(start_time+manageable_timeout)):
+            print '----',to_stop or (time.time()>(start_time+manageable_timeout))
             time.sleep(5)
             interface_vlan = get_switch_conf_as_json(setup_params['switch_ip'], setup_params['switch_user'],setup_params['switch_password'],setup_params['switch_type'])['InterfaceVlan']
             actual_vlans=[]
@@ -166,8 +167,8 @@ class AnsibleNetworkingFunctionalityTests(unittest.TestCase):
         self.assertEqual(str(actual_vlans).count(str(baremetal_vlan_id)),2, 'Failed: baremetal ports are set to incorrect vlans:\n'+str(actual_vlans))
         start_time = time.time()
         to_stop=False
-        while to_stop==False or (time.time()>(start_time+available_timeout)):
-            print to_stop
+        while to_stop or (time.time()>(start_time+available_timeout)):
+            print '----', to_stop or (time.time() > (start_time + manageable_timeout))
             time.sleep(5)
             states = [item['provisioning state'] for item in exec_command_line_command(source_overcloud + 'openstack baremetal node list -f json')['JsonOutput']]
             if states==['available','available']:
