c4a6bfa8ef160c89a6de28af41fd45d1a3561649
Arkady Shtempler <ashtempl@redhat.com>
2019-01-02 14:00:46 +0200

    First push

--- Get_Juniper_Switch_Info.py
+++ Get_Juniper_Switch_Info.py
@@ -5,7 +5,7 @@ setup_parameters={
     'baremetal_guest_ports':['xe-0/0/6', 'xe-0/0/7'],
     'switch_type':'juniper_physical_sw',
     'switch_ip':'10.9.95.25',
-    'switch_user':'root',
+    'switch_user':'ansible',
     'switch_password':'N3tAutomation!',
     'tenant_nets':['tenant-net','tenant-net2'],
     'setup':'QE_Setup'
