b79565fc2762f456a318da0cf8b701f6dfdfe09c
Arkady Shtempler <ashtempl@redhat.com>
2019-01-02 13:24:39 +0200

    First push

--- Common.py
+++ Common.py
@@ -159,15 +159,15 @@ def get_switch_conf_as_json(ip,user,password,sw_type=None):
 def get_juniper_sw_get_port_vlan(ip, user, password, port):
     ssh_object = SSH(ip, user, password)
     ssh_object.ssh_connect_password()
-    out = ssh_object.ssh_command_only('show ethernet-switching interface '+port)['Stdout'].split('\n')
-    print out
+    out = ssh_object.ssh_command_only('show ethernet-switching table')['Stdout']
     ssh_object.ssh_close()
-    print out[0]
-    tag_start=out[0].find('TAG')
-    mac_start=out[0].find('MAC')
-    print tag_start,mac_start
-    vlan_tags=[line[tag_start:mac_start].replace(' ','') for line in out[1:]]
-    print vlan_tags
+
+    for line in out.split('\n'):
+        print line
+
+
+    #vlan_tags=[line[tag_start:mac_start].replace(' ','') for line in out[1:]]
+    #print vlan_tags
 
 
 
