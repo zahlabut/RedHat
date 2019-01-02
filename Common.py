11c03432dfb0d0e2f5bea8f4254594a7916eb8ce
Arkady Shtempler <ashtempl@redhat.com>
2019-01-02 13:29:19 +0200

    First push

--- Common.py
+++ Common.py
@@ -166,6 +166,9 @@ def get_juniper_sw_get_port_vlan(ip, user, password, port):
         if port in line:
             print line
 
+    lines=[line.split(' ') for line in out.split('\n') if port in line]
+    print lines
+
 
 
     #vlan_tags=[line[tag_start:mac_start].replace(' ','') for line in out[1:]]
