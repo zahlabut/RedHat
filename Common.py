ada54a0c7afd3719a210877fe86212e382557c37
Arkady Shtempler <ashtempl@redhat.com>
2019-01-03 13:02:39 +0200

    First push

--- Common.py
+++ Common.py
@@ -164,9 +164,9 @@ def get_juniper_sw_get_port_vlan(ip, user, password, ports):
         command='show configuration interfaces '+port
         out=ssh_object.ssh_command_only(command)['Stdout']
         print out
-        vlans=[line.split('members')[1].strip(';').replace(']','').replace('[','') for line in out.split('\n') if 'members' in line]
-        print vlans
-        print eval(vlans)
+        # vlans=[line.split('members')[1].strip(';').replace(']','').replace('[','') for line in out.split('\n') if 'members' in line]
+        # print vlans
+        # print eval(vlans)
     #     print vlans[0]
     #     vlans=list(set(vlans))
     #     result_dic[port]=vlans
