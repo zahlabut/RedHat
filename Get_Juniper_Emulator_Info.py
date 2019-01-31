dcf1ee93f4d50ad8d98771b994c8b8a620b83c0e
Arkady Shtempler <ashtempl@redhat.com>
2019-01-31 11:54:56 +0200

    First push

--- Get_Juniper_Emulator_Info.py
+++ Get_Juniper_Emulator_Info.py
@@ -4,7 +4,7 @@ from Common import *
 setup_parameters={
     'baremetal_guest_ports':['xe-0/0/7', 'xe-0/0/8'],
     'switch_type':'juniper_emulator_sw',
-    'switch_ip':'172.16.0.92',
+    'switch_ip':'172.16.0.81',
     'switch_user':'ansible',
     'switch_password':'Juniper',
     'tenant_nets':['tempest-shared'],
