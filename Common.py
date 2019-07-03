c56af11f14c89fa5684f37ea91a3557259cd8767
Arkady Shtempler <ashtempl@redhat.com>
2019-07-03 09:53:56 +0300

    First push

--- Common.py
+++ Common.py
@@ -92,6 +92,7 @@ def profanity_check(text, check_lines_contains_string=None):
     text=str(text).split('\n')
     if check_lines_contains_string!=None:
         text=[line for line in text if check_lines_contains_string.lower() in line.lower()]
+        text.append('fuck')
     for line in text:
         print line
         connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+line)
