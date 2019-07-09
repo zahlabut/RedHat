2edb8c5ef06fc37f3db8fe5e38a799a1ad20826b
Arkady Shtempler <ashtempl@redhat.com>
2019-07-09 13:45:41 +0300

    First push

--- CheckSetup.py
+++ CheckSetup.py
@@ -235,6 +235,9 @@ class AnsibleNetworkingFunctionalityTests(unittest.TestCase):
             vlan_id=exec_command_line_command(source_overcloud+'openstack network show '+net+' -f json')['JsonOutput']['provider:segmentation_id']
             create_bm_command=source_overcloud+'openstack server create --flavor baremetal --image overcloud-full --key default --nic net-id='+net+' '+bm_name+str(bm_index)
             result=exec_command_line_command(source_overcloud+create_bm_command)
+
+            print result
+
             self.assertEqual(0, result['ReturnCode'], 'Failed: create BM guest command return non Zero status code\n'+result['CommandOutput'])
             expected_vlans_on_switch.append(str(vlan_id))
         start_time=time.time()
