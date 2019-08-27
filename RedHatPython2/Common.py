7074d9b7a5d3b70b97ad9b8720e8274eb3987995
Arkady Shtempler <ashtempl@redhat.com>
2019-08-27 12:56:33 +0300

    First push

--- RedHatPython2/Common.py
+++ RedHatPython2/Common.py
@@ -111,7 +111,7 @@ def exec_command_line_command(command):
         return {'ReturnCode': 0, 'CommandOutput': result, 'JsonOutput': json_output}
     except subprocess.CalledProcessError as e:
         print e
-        return {'ReturnCode': e.returncode, 'CommandOutput': 'Failed to execute: \n'+command+'with:\n'+str(e)}
+        return {'ReturnCode': e.returncode, 'CommandOutput': 'Failed to execute: \n'+command+'with:\n'+e.output}
 
 
 def profanity_check(text, check_lines_contains_string=None):
