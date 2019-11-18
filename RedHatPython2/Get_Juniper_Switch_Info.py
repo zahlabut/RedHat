61803136ec0995d327c097b18e3da3fc320b9a1e
Arkady Shtempler <ashtempl@redhat.com>
2019-11-18 11:27:09 +0200

    First push

--- RedHatPython2/Get_Juniper_Switch_Info.py
+++ RedHatPython2/Get_Juniper_Switch_Info.py
@@ -21,9 +21,9 @@ for k in vlans.keys():
     print k,' --> ', vlans[k]
 
 
-print run_command_on_switch(
-    setup_parameters['switch_ip'],
-    setup_parameters['switch_user'],
-    setup_parameters['switch_password'],
-    #command='edit;delete interfaces xe-0/0/7 unit 0 family ethernet-switching vlan members 1202;commit')
-    command='edit;set interfaces xe-0/0/7 unit 0 family ethernet-switching vlan members 1202;commit')
+# print run_command_on_switch(
+#     setup_parameters['switch_ip'],
+#     setup_parameters['switch_user'],
+#     setup_parameters['switch_password'],
+#     #command='edit;delete interfaces xe-0/0/7 unit 0 family ethernet-switching vlan members 1202;commit')
+#     command='edit;set interfaces xe-0/0/7 unit 0 family ethernet-switching vlan members 1202;commit')
